7b7e81fcd4107a695584bdbbf28af4e4023b7d0e
me <me@robertcurtiss.com>
2024-04-02 19:57:56 -0400

    Monday 04-02

--- django_project/blog/forms.py
+++ django_project/blog/forms.py
@@ -1,14 +1,35 @@
+import datetime
+from email.policy import default
+
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django.forms import DateField, DateInput, ModelForm
+
+# from django_project.users import forms
+from django.utils import timezone
 
 from .models import Post
 
 
 class PostForm(ModelForm):
+    # published_at = forms.DateField(
+    #     widget=forms.DateInput(
+    #         attrs={"type": "date"},
+    #     ),
+    # )
+    # published_at.initial = default = timezone.now
+    # content = forms.CharField(widget=forms.Textarea(attrs={"rows": 4, "cols": "40"}))
+
     class Meta:
         model = Post
-        fields = ["title", "content",]
+        fields = [
+            "title",
+            "content",
+        ]
+        # https://docs.djangoproject.com/en/5.0/topics/forms/modelforms/
+        widgets = {"content": forms.Textarea(attrs={"rows": 4, "cols": "40"})}
+        labels = {"content": "Detailed Description"}
 
 
 class RegisterForm(UserCreationForm):
--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,13 +1,18 @@
 # Create your models here.
+from email.policy import default
+from typing import Any
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 
 class Post(models.Model):
+
     title = models.CharField(max_length=120)
     content = models.TextField()
     published_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
